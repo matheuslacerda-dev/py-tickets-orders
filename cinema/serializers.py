8a52e2e6a4c2285e2c5ae98217e7c922a6c16808
matheuslacerda-dev <matheuslacerda01@proton.me>
2026-05-15 15:33:22 -0300

    fix line length

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -37,7 +37,9 @@ class MovieSerializer(serializers.ModelSerializer):
 
 
 class MovieListSerializer(MovieSerializer):
-    genres = serializers.SlugRelatedField(many=True, read_only=True, slug_field="name")
+    genres = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="name"
+    )
     actors = serializers.SlugRelatedField(
         many=True, read_only=True, slug_field="full_name"
     )
@@ -60,7 +62,9 @@ class MovieSessionSerializer(serializers.ModelSerializer):
 
 class MovieSessionListSerializer(MovieSessionSerializer):
     movie_title = serializers.CharField(source="movie.title", read_only=True)
-    cinema_hall_name = serializers.CharField(source="cinema_hall.name", read_only=True)
+    cinema_hall_name = serializers.CharField(
+        source="cinema_hall.name", read_only=True
+    )
     cinema_hall_capacity = serializers.IntegerField(
         source="cinema_hall.capacity", read_only=True
     )
@@ -98,7 +102,9 @@ class TicketSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         rep = super().to_representation(instance)
-        rep["movie_session"] = MovieSessionListSerializer(instance.movie_session).data
+        rep["movie_session"] = MovieSessionListSerializer(
+            instance.movie_session
+        ).data
         return rep
 
 
